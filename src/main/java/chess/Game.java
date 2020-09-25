ae47b8467e631744d407b40485d463cb8fb73404
David Hernández Uriostegui <Dhdezu@ciencias.unam.mx>
2020-09-24 22:42:12 -0500

    Uploading all files...

---
+++ src/main/java/chess/Game.java
@@ -0,0 +1,12 @@
+package chess;
+import chess.items.Board;
+import chess.pieces.ColorEnum;
+import chess.pieces.Piece;
+import chess.pieces.chessPieces.Rook;
+import chess.items.*;   
+public class Game{
+
+    public static void main(String[] args) {
+       // Board board = Board.getInstance();
+    }
+}
